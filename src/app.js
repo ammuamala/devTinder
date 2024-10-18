3816bbf7c811203acae7eef8237453851601a112
ammuamala <amalahr1997@gmail.com>
2024-10-18 22:11:41 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -10,15 +10,12 @@ app.get('/user',(req,res)=>{
     res.send({firstName:"amala",lastname:"polisetty"})
 });
 
-
-
 app.post('/user',(req,res)=>{
     res.send("Data successfully saved to the database")
 })
 app.delete('/user',(req,res)=>{
     res.send("deleted succussfully")
 })
-
 
 app.listen(3000,()=>{
     console.log("server is succussfully listining on 3000...")
