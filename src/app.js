4081b359f1bb7f021322f9e38ec32f4b89282325
ammuamala <amalahr1997@gmail.com>
2024-10-18 22:09:19 +0530

    Exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ const express=require("express");
 
 const app=express()
 
-app.use('/test',(req,res)=>{
-    res.send("hello from the server")
+app.use('/user',(req,res)=>{
+    res.send("abrakadabra")
+})
+
+app.get('/user',(req,res)=>{
+    res.send({firstName:"amala",lastname:"polisetty"})
 });
 
-app.use('/hello',(req,res)=>{
-    res.send("hello hello hello")
+
+
+app.post('/user',(req,res)=>{
+    res.send("Data successfully saved to the database")
 })
+app.delete('/user',(req,res)=>{
+    res.send("deleted succussfully")
+})
+
 
 app.listen(3000,()=>{
     console.log("server is succussfully listining on 3000...")
