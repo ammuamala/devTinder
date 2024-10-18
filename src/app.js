6c3a712e553af75e5aca12f60cca8d2c939fe9bc
ammuamala <amalahr1997@gmail.com>
2024-10-18 23:25:24 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,9 @@ const express=require("express");
 
 const app=express()
 
-app.use('/user',(req,res)=>{
-    res.send("abrakadabra")
-})
-
-app.get('/user',(req,res)=>{
-    res.send({firstName:"amala",lastname:"polisetty"})
-});
-
-app.post('/user',(req,res)=>{
-    res.send("Data successfully saved to the database")
-})
-app.delete('/user',(req,res)=>{
-    res.send("deleted succussfully")
+app.get('/user/:userId/:password/:name',(req,res)=>{
+    console.log(req.params)
+    res.send({firstName:'amala',lastName:'polisetty'})
 })
 
 app.listen(3000,()=>{
