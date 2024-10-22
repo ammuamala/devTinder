c70d38990897b3205569318f699580c5e51c7db8
ammuamala <amalahr1997@gmail.com>
2024-10-22 19:59:50 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -2,19 +2,14 @@ const express=require("express");
 
 const app=express()
 
-app.use('/user',(req,res)=>{
-    res.send("abrakadabra")
-})
-
-app.get('/user',(req,res)=>{
-    res.send({firstName:"amala",lastname:"polisetty"})
-});
-
-app.post('/user',(req,res)=>{
-    res.send("Data successfully saved to the database")
-})
-app.delete('/user',(req,res)=>{
-    res.send("deleted succussfully")
+app.use("/user",(req,res,next)=>{
+    console.log("request handler 1");
+    res.send("response 1");
+    next();
+},(req,res,next)=>{
+    console.log("request handler 2");
+    res.send("response 2");
+    next();
 })
 
 app.listen(3000,()=>{
