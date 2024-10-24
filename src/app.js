ab3614b83f4add1db7ee8f7183236e834ab1c6d7
ammuamala <amalahr1997@gmail.com>
2024-10-24 20:57:56 +0530

    Create-post-api call

--- src/app.js
+++ src/app.js
@@ -1,25 +1,34 @@
 const express=require("express");
-
+const connectDB=require("./config/database")
 const app=express()
+const mongoose=require("mongoose");
+const User=require("./models/user")
 
-const {adminAuth,userAuth}=require("./middleware/auth")
-
-app.use('/admin',adminAuth,userAuth)
-
-app.get('/user',(req,res)=>{
-    res.send("get all users")
-})
-
-app.get('/admin/getAlldata',(req,res)=>{
-    res.send("all data sent")
-   
+app.post("/signup",async(req,res)=>{
+    //creat a new instance of the user model
+    const user=new User({
+        firstName:"krishna",
+        lastName:"pabolu",
+        emailId:"pabolu.krishna@gmail.com",
+        password:"jdshla132",
+    })
+    try{
+        await user.save()
+        res.send("user added successfaully")
+    }catch(err){
+        res.status(400).send("Error saving the user:"+ err.message)
+    }
     
-});
-app.get("/admin/deleteUser",(req,res)=>{
-   res.send("deleted a user");
-   
 })
 
-app.listen(3000,()=>{
-    console.log("server is succussfully listining on 3000...")
+
+connectDB()
+.then(()=>{
+    console.log("database connection established...")
+    app.listen(3000,()=>{
+        console.log("server is succussfully listining on 3000...")
+    })
+
+}).catch((err)=>{
+    console.log("database cannot be connected")
 })
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose= require('mongoose');
+
+
+const connectDB=async() => {
+    await mongoose.connect(
+        "mongodb+srv://polisettyamala:Ammuamala@namastenode.ibj8g.mongodb.net/devTinder"
+    );
+};
+
+
+
+module.exports=connectDB
