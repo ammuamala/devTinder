3ad862275a8161664e8cf75914b90ff91424dcd1
ammuamala <amalahr1997@gmail.com>
2024-11-04 20:15:53 +0530

    SignUp-Login

--- src/app.js
+++ src/app.js
@@ -4,20 +4,69 @@ const app=express()
 const mongoose=require("mongoose");
 const User=require("./models/user");
 const { ReturnDocument } = require("mongodb");
+const{validateSignUpData}=require("./utils/validation")
+// const cookieParser=require("cookie-parser")
 
 app.use(express.json())
+// app.use(cookieParser())
+
+const bcrypt=require("bcrypt")
 
 app.post("/signup",async(req,res)=>{
-   
-    // //creat a new instance of the user model
-    const user=new User(req.body)
     try{
-        await user.save()
-        res.send("user added successfaully")
+      //validate of data
+      validateSignUpData(req);
+
+      const {firstName,lastName,emailId,password}=req.body;
+
+      //Encrypt the password
+      const passwordHash=await bcrypt.hash(password,10);
+      console.log(passwordHash)
+ 
+      //creat a new instance of the user model
+       const user=new User({
+        firstName,
+        lastName,
+        emailId,
+        password:passwordHash
+       })
+
+       await user.save()
+       res.send("user added successfaully")
     }catch(err){
         res.status(400).send("Error saving the user:"+ err.message)
     }
 })
+    
+   
+app.post("/login",async(req,res)=>{
+    try{
+        const {emailId,password}=req.body;
+
+        const user=await User.findOne({emailId:emailId})
+        if(!user){
+            throw new Error("Invalid emailId")
+        }
+        const isPasswordvalid =await bcrypt.compare(password,user.password)
+
+        if(isPasswordvalid){
+            // res.cookie("token","abcdefhidoajfakddddddddmadmowierjkjdsnf")
+            // res.send("login succusfully")
+        }else{
+            throw new Error("Invalid password")
+        }
+    }catch(err){
+        res.status(400).send("Error saving the user:"+ err.message)
+    }
+})
+
+// app.get("/profile",async(req,res)=>{
+//     const cookie=req.cookies;
+
+//     console.log(cookie);
+//     res.send("Reading cookies")
+// })
+   
 
 //Get user by email
 app.get('/user',async(req,res)=>{
@@ -70,19 +119,28 @@ app.delete("/user",async(req,res)=>{
 })
 
 //update data of the user
-app.patch("/user",async(req,res)=>{
-    const userId=req.body.userId;
+app.patch("/user/:userId",async(req,res)=>{
+    const userId=req.params?.userId;
     const data=req.body;
 
-    try{
-        const user=await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after"})
-        console.log(user)
-        res.send("user updated successfully")
+    const ALLOWED_UPDATES=["userId","photoUrl","firstName","lastName","skills","age","gender"]
+    const isUpdatedAllowed=Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k))
+
+    if(!isUpdatedAllowed){
+        res.status(400).send("update not allowed")
     }
-   
-    catch(err){
+    if(data?. skills. length > 10){ 
+        throw new Error("skills or not more than 10")
+     }
+    try{
+        const user=await User.findByIdAndUpdate({_id:userId},data,{runValidators:true},)
+        
+        res.send("user updated successfully")
+       
+    }catch(err){
         res.status(400).send('something went wrong')
     }
+
 })
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,55 @@
 const mongoose = require("mongoose")
+const validator=require("validator")
 
 const userSchema= mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
-    },
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        validate: {
+            validator: validator.isEmail, // Use validator.isEmail directly here
+            message: "Invalid email address"
+          }
+        },
     password:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        
     },
     age:{
         type:Number
     },
     gender:{
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new error("gender data is not valid")
+                
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://pixabay.com/images/search/user/",
+    },
+    about:{
         type:String
     },
+    skills:{
+        type:[String],
+      
+    },
+},{
+    timestamps:true
 })
 
 module.exports=mongoose.model("User",userSchema)
