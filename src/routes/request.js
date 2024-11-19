7b31706311f6ddfec5ced380ac8e15d357a58c3c
ammuamala <amalahr1997@gmail.com>
2024-11-19 19:10:22 +0530

    /request/review

--- src/routes/request.js
+++ src/routes/request.js
@@ -47,4 +47,32 @@ requestRouter.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
     }
 })
 
+requestRouter.post("/request/review/:status/:requestId",userAuth,async(req,res)=>{
+    try{
+        const loggedInuser=req.user;
+        const {status,requestId}=req.params
+
+        const allowedStatus=["accepted","rejected"];
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message:"status not allowed"})
+        }
+         const connectionRequest=await ConnectionRequest.findOne({
+            _id:requestId,
+            toUserId:loggedInuser._id,
+            status:"interested"
+         })
+         if(!connectionRequest){
+            return res.status(400).json({message:"connection request not found"})
+         }
+         connectionRequest.status=status
+
+         const data=await connectionRequest.save();
+
+         res.json({message:"connection request "+status,data});
+
+    }catch(err){
+        res.status(400).send("ERROR: "+err.message);
+    }
+})
+
 module.exports=requestRouter
